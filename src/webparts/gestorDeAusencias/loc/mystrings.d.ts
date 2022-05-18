96068a6184e7d688082b805b67861ad20b65e898
Andrés Eduardo García Márquez <andresgarcia0313@gmail.com>
2022-05-18 00:15:30 -0500

    Versión Inicial SPFX React

---
+++ src/webparts/gestorDeAusencias/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IGestorDeAusenciasWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'GestorDeAusenciasWebPartStrings' {
+  const strings: IGestorDeAusenciasWebPartStrings;
+  export = strings;
+}
