39bb4285d2fb4c7f8048b8f5bbf61cadcfd6476c
Andres Eduardo Garcia Marquez <andresgarcia0313@gmail.com>
2022-09-23 18:44:40 -0500

    se quita código no necesario y algunas interfacez

--- src/webparts/gestorDeAusencias/GestorDeAusenciasWebPart.ts
+++ src/webparts/gestorDeAusencias/GestorDeAusenciasWebPart.ts
@@ -1,23 +1,14 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import { IPropertyPaneConfiguration, PropertyPaneTextField } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-import * as strings from 'GestorDeAusenciasWebPartStrings';
 import GestorDeAusencias from './components/GestorDeAusencias';
-import { IGestorDeAusenciasProps } from './components/IGestorDeAusenciasProps';
-import { getRandomString } from "@pnp/core";
-export interface IGestorDeAusenciasWebPartProps {
-  description: string;
-}
-export default class GestorDeAusenciasWebPart extends BaseClientSideWebPart<IGestorDeAusenciasWebPartProps> {
+export default class GestorDeAusenciasWebPart extends BaseClientSideWebPart<any> {
   private _isDarkTheme: boolean = false;
   protected async onInit(): Promise<void> { return await super.onInit(); }
   public render(): void {
     console.clear();//limpia la consola
-    //(() => { console.log("PNPJS instalado: " + getRandomString(20)) })()// Valida que este instalado la librería pnpjs
-    const element: React.ReactElement<IGestorDeAusenciasProps> = React.createElement(
+    const GestorDeAusenciasElement: React.ReactElement<any> = React.createElement(
       GestorDeAusencias, {
       description: this.properties.description,
       isDarkTheme: this._isDarkTheme,
@@ -25,19 +16,19 @@ export default class GestorDeAusenciasWebPart extends BaseClientSideWebPart<IGes
       userDisplayName: this.context.pageContext.user.displayName,
       user:this.context.pageContext.user,
       context: this.context
-    });//Creaciónn de objeto react con sus props o propiedades    
-    ReactDom.render(element, this.domElement);//Renderiza en elemento    
+    });//Creación de objeto react con sus props o propiedades    
+    ReactDom.render(GestorDeAusenciasElement, this.domElement);//Renderiza en elemento    
   }
 
-  private _getEnvironmentMessage(): string {
+  /*private _getEnvironmentMessage(): string {
     if (!!this.context.sdks.microsoftTeams) { // running in Teams
       return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
     }
 
     return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
-  }
+  }*/
 
-  protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
+  /*protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
     if (!currentTheme) {
       return;
     }
@@ -50,13 +41,13 @@ export default class GestorDeAusenciasWebPart extends BaseClientSideWebPart<IGes
     this.domElement.style.setProperty('--link', semanticColors.link);
     this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
 
-  }
+  }*/
 
   protected onDispose(): void { ReactDom.unmountComponentAtNode(this.domElement); }
 
   protected get dataVersion(): Version { return Version.parse('1.0'); }
 
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+  /*protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
@@ -74,5 +65,5 @@ export default class GestorDeAusenciasWebPart extends BaseClientSideWebPart<IGes
         }
       ]
     };
-  }
+  }*/
 }
